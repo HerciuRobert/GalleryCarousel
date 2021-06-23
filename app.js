56dca56f6813acc9ef425ecd419bc20a18564886
RobertH <herciu.robert@gmail.com>
2021-06-23 21:50:03 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+let list = document.querySelector(".list");
+let imgs = Array.from(list.children);
+const nextBtn = document.querySelector(".btn-right");
+const prevBtn = document.querySelector(".btn-left");
+
+
+//Getting img width
+const imgWidth = imgs[0].getBoundingClientRect().width;
+//Arranging imgs next to each other
+
+function setImgPosition(img, index) {
+    img.style.left = imgWidth * index + "px";
+}
+
+imgs.forEach(setImgPosition);
+
+// Move to Img function
+
+const moveToImg = function(list, currentImg, targetImg) {
+    list.style.transform = "translateX(-" + targetImg.style.left + ")";
+    currentImg.classList.remove("current-img");
+    targetImg.classList.add("current-img");
+};
+
+// Hide/Show extremity buttons
+
+const hideShowArrows = function(imgs, prevBtn, nextBtn, targetIndex) {
+    if(targetIndex === 0) {
+        prevBtn.classList.add("hidden");
+        nextBtn.classList.remove("hidden");
+    } else if(targetIndex === imgs.length -1) {
+        prevBtn.classList.remove("hidden");
+        nextBtn.classList.add("hidden");
+    } else {
+        prevBtn.classList.remove("hidden");
+        nextBtn.classList.remove("hidden");
+    }
+}
+
+// Click right move images to right
+
+nextBtn.addEventListener("click", function() {
+    const currentImg = list.querySelector(".current-img");
+    const nextImg = currentImg.nextElementSibling;
+    const nextIndex = imgs.findIndex((img) => img === nextImg);
+
+    moveToImg(list, currentImg, nextImg);
+    hideShowArrows(imgs, prevBtn, nextBtn, nextIndex);
+});
+
+// Click left move images to left
+
+prevBtn.addEventListener("click", function() {
+    const currentImg = list.querySelector(".current-img");
+    const prevImg = currentImg.previousElementSibling;
+    const prevIndex = imgs.findIndex((img) => img === prevImg);
+
+    moveToImg(list, currentImg, prevImg);
+    hideShowArrows(imgs, prevBtn, nextBtn, prevIndex);
+});
